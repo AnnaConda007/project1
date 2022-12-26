8e316237a3de2c817ca5bb0ce01312cdda496220
AnnaConda007 <annahrulkova@yandex.ru>
2022-12-26 16:58:02 +0300

    произведено форматирование

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,56 +1,56 @@
-const modals = function(){    /* это функциональное выражение. Ссылка на ее содержимое(сама функция) 
+const modals = function () {
+  /* это функциональное выражение. Ссылка на ее содержимое(сама функция) 
 передается в main.js и далее вызывается в функции при полной загрузке DOM*/
-  
-function bindModal(triggerSelector, modalSelector, closeSelector){
-        const triggers = document.querySelectorAll(triggerSelector)
-        const  modal = document.querySelector(modalSelector)
-        const close = document.querySelector(closeSelector)
-        triggers.forEach((trigger)=>{
-            trigger.addEventListener("click", (e)=>{
-                if(e.target){
-                 e.preventDefault()
-                }
-                modal.style.display = "block"
-                document.body.classList.add("modal-open") 
-            })
-        })
 
-        const closeModal = ()=>{
-            modal.style.display = "none"
-            document.body.classList.remove("modal-open") 
+  function bindModal(triggerSelector, modalSelector, closeSelector) {
+    const triggers = document.querySelectorAll(triggerSelector)
+    const modal = document.querySelector(modalSelector)
+    const close = document.querySelector(closeSelector)
+    triggers.forEach((trigger) => {
+      trigger.addEventListener('click', (e) => {
+        if (e.target) {
+          e.preventDefault()
         }
-            window.addEventListener("keydown", (e)=>{ 
-                if (e.code == "Escape")   {
-            closeModal()}})
-            close.addEventListener("click", ()=>{ 
-                    closeModal()})
-            modal.addEventListener("click", ()=>{ 
-                        closeModal()})
-    
-    const showModalByTime = (selector, time)=>{
-        setTimeout(function(){
-            document.querySelector(selector).style.display = "block"
-            document.body.classList.remove("modal-open")
-        },time)
+        modal.style.display = 'block'
+        document.body.classList.add('modal-open')
+      })
+    })
+
+    const closeModal = () => {
+      modal.style.display = 'none'
+      document.body.classList.remove('modal-open')
     }
-    showModalByTime(".popup", 60000) 
+    window.addEventListener('keydown', (e) => {
+      if (e.code == 'Escape') {
+        closeModal()
+      }
+    })
+    close.addEventListener('click', () => {
+      closeModal()
+    })
+    modal.addEventListener('click', () => {
+      closeModal()
+    })
+
+    const showModalByTime = (selector, time) => {
+      setTimeout(function () {
+        document.querySelector(selector).style.display = 'block'
+        document.body.classList.remove('modal-open')
+      }, time)
+    }
+    showModalByTime('.popup', 60000)
+  }
+
+  bindModal(
+    '.popup_engineer_btn',
+    '.popup_engineer',
+    '.popup_engineer .popup_close',
+  )
+  bindModal('.phone_link', '.popup', '.popup .popup_close')
 }
+export default modals // команда для работы с "упаковщиками", не влияет на работу самого модального окна
 
-    bindModal(".popup_engineer_btn", ".popup_engineer", ".popup_engineer .popup_close")
-    bindModal(".phone_link", ".popup", ".popup .popup_close")
-   
-
-}
-export default modals // команда для работы с "упаковщиками", не влияет на работу самого модального окна 
- 
-
-
-
-
-
-
-
- /* при вызове showModalByTime автоматически появляется модульное окно. 
+/* при вызове showModalByTime автоматически появляется модульное окно. 
     Одновременно с загрузкой старницы запускается функция bindModal, где задаются события клику на крестик и 
     подложку формы*/
 /* 
