30a824e6fe993abc92b1085c888fb3c68a242a3a
AnnaConda007 <annahrulkova@yandex.ru>
2023-01-09 13:42:29 +0400

    для слияния

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -73,26 +73,27 @@ const modals = function () {
 
     showModalByTime('.popup_engineer', 60000)
   }
+}
 
-  bindModal(
-    ".popup_engineer_btn",
-    ".popup_engineer",
-    ".popup_engineer .popup_close"
-  );
-  bindModal(".phone_link", ".popup", ".popup .popup_close");
-  bindModal(".popup_calc_btn", ".popup_calc", ".popup_calc_close");
-  bindModal(
-    ".popup_calc_button",
-    ".popup_calc_profile",
-    ".popup_calc_profile_close",
-    false
-  );
-  bindModal(
-    ".popup_calc_profile_button",
-    ".popup_calc_end",
-    ".popup_calc_end_close",
-    false
-  );
+bindModal(
+  ".popup_engineer_btn",
+  ".popup_engineer",
+  ".popup_engineer .popup_close"
+);
+bindModal(".phone_link", ".popup", ".popup .popup_close");
+bindModal(".popup_calc_btn", ".popup_calc", ".popup_calc_close");
+bindModal(
+  ".popup_calc_button",
+  ".popup_calc_profile",
+  ".popup_calc_profile_close",
+  false
+);
+bindModal(
+  ".popup_calc_profile_button",
+  ".popup_calc_end",
+  ".popup_calc_end_close",
+  false
+);
 };
 export default modals; // команда для работы с "упаковщиками", не влияет на работу самого модального окна
 
